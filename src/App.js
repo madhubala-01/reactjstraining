a7c9fd98355f47be9648bc5195a7eaf60f670de4
madhubala <madhubala.knetworks@gmail.com>
2020-12-14 14:32:50 +0530

    form created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+        <header><strong><center>Registration Form</center></strong></header>
+        <form>
+         
+          <label>Name</label>
+          <input type = "text" /><br></br>
+          <label>Date of Birth</label>
+          <input type = "date" /><br></br>
+          <label>Blood Group</label>
+          <select>
+            <option>A +ve</option>
+            <option>A -ve</option>
+            <option>B +ve</option>
+            <option>B -ve</option>
+            <option>AB +ve</option>
+            <option>AB -ve</option>
+            <option>O +ve</option>
+            <option>O -ve</option>
+          </select><br></br>
+          <label>Gender</label>
+          <input type ="radio" name="gender" />Male
+          <input type ="radio" name="gender" />Female
+          <input type ="radio" name="gender" />Transgender <br></br>
+          <label>Phone number</label>
+          <input type ="text" /><br></br>
+          <label>Email Id</label>
+          <input type= "text" /><br></br>
+          <center><input type= "submit" />
+          
+          </center>
+        </form>
+      
+     
     </div>
   );
 }
